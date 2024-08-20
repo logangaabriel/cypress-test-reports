56fc8ce006d0012a46e51226d8a32cddde358311
gaabrielLogan <gabriel.logan@atlanteti.com>
2024-08-20 14:48:58 -0300

    feat: integrate JUnit for Cypress test reporting and add relevant scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mocha-junit-reporter',
+  reporterOptions: {
+    mochaFile: 'results/test-results-[hash].xml',
+    toConsole: true,
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+   
   },
 });
