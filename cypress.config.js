eda8a33189404795161f0e3dacc8aab62cff861e
gaabrielLogan <gabriel.logan@atlanteti.com>
2024-08-20 13:40:38 -0300

    feat: integrate Mochawesome for Cypress test reporting and add relevant scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
